cdef465a7d3a64926759ea9c3cd29ea02272fc4a
fulano <el.iagows@gmail.com>
2020-04-01 19:29:22 -0300

    removed cloning
    sem necessidade

--- js/iagows.js
+++
@@ -1,9 +0,0 @@
-function copyMe(argument) {
-	var copyText = document.getElementById(argument);
-	
-	var text = copyText.select();
-	copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-	document.execCommand("copy");
-
-	copyText.setAttribute("value", "Copied");
-}
