5c1385f93a88dc1e807ec61217ebd3c26ba61a04
fulano <el.iagows@gmail.com>
2020-04-01 19:23:23 -0300

    js fix

--- js/iagows.js
+++ js/iagows.js
@@ -2,7 +2,7 @@ function copyMe(argument) {
 	var copyText = document.getElementById(argument);
 	
 	var text = copyText.select();
-	text.setSelectionRange(0, 99999); /*For mobile devices*/
+	copyText.setSelectionRange(0, 99999); /*For mobile devices*/
 	document.execCommand("copy");
 
 	copyText.setAttribute("value", "Copied");
